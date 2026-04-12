8e9bc7b5bedc30eb482edca7eb3b9ab661beeda7
Daniel Rakotomalala <rakdanielh@gmail.com>
2026-04-12 22:56:46 +0300

    refactory

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,7 @@
 globalVariables(unique(c(
   "origin", "destination",
   ".", "datasets", "nvar", "nobs",
-  "to_convert", "idvarsdupl")))
+  "to_convert", "idvarsdupl",
+  "nb_distinct", "nb_na", "nb_void",
+  "input_vector"
+)))
---
+++ tests/testthat/test-assign_to_global.R
@@ -0,0 +1,2 @@
+# assign_to_global has been removed from the package.
+# This file is intentionally left with no tests.
